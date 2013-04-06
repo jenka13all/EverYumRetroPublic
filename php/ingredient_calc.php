41f62dd708c1368e24d0e263aa7e1dd717610c19
jenka13all <jenka13all@gmail.com>
2013-04-07 00:35:00 +0200

    json data from text file switched out to json data returned by A's code

--- php/ingredient_calc.php
+++ php/ingredient_calc.php
@@ -13,11 +13,38 @@
 <body>
 	
 <?php
+date_default_timezone_set('UTC');
+
 include_once("../libs/ingredient_calc_functions.php");
+//require_once '../vendor/autoload.php';
+
+$loader = require '../vendor/autoload.php';
+
+$config = include __DIR__ . '/../config.php';
+$app = new \EverYum\Application($config);
+
+$ingredients = [
+    'pepper',
+    'salt',
+    'zucchini',
+    'mango',
+    'garlic',
+    'chocolate',
+    'marmelade',
+    'asparagus',
+];
+$course = [
+    'course^course-Main Dishes'
+];
+$diet = [
+    '386^Vegan',
+];
 
 //replace with EverNote results
-$fridge_list = "corn, peas, carrots, tomato, lentils, tomato paste, tofu, lemon, capers";
+//$fridge_list = "corn, peas, carrots, tomato, lentils, tomato paste, tofu, lemon, capers";
 $grocery_list = "soy milk, onion, garlic, bannana, lime, cashew, tahini";
+$fridge_list = implode(",", $ingredients);
+//echo "fridge_list = $fridge_list <br>";
 
 $arr_my_items = return_have_items_array($fridge_list, $grocery_list);
 $my_items_count = count($arr_my_items);
@@ -43,12 +70,20 @@ What to buy in order to make...
 
 <?php
 //replace filename with json data variable
-$arr_recipes = return_recipe_array("../libs/yumly_jsonp.json");
+//$arr_recipes = return_recipe_array("../libs/yumly_jsonp.json");
 
+//this is an object...
+//$test_recipe =  $app->service['yummly']->getRecipe("Vegan-Bulgur-Chili-Naturally-Ella-46337");
+//echo "test recipe = $test_recipe <br>";
+
+$arr_recipes = $app->service['yummly']->getRecipesByIngredients($ingredients, $course, $diet);
+
+//foreach($arr_recipes->matches as $recipe) 
 ?>
 
 <?php
-$arr_tobuy = return_shopping_list($arr_my_items, $arr_recipes);
+//$arr_tobuy = return_shopping_list($arr_my_items, $arr_recipes);
+$arr_tobuy = return_shopping_list_obj($arr_my_items, $arr_recipes);
 
 //show what it can do
 foreach ($arr_tobuy as $recipe => $shopping_list)
