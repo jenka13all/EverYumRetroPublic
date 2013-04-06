8e9af8231be403cddc64b7294a386f5042f24313
jenka13all <jenka13all@gmail.com>
2013-04-06 10:01:55 +0200

    added dirs

---
+++ php/index.php
@@ -0,0 +1,32 @@
+<?php 
+error_reporting(E_ALL); 
+//rhc tail EverYumRetro to see errors
+//  neeed to cd into every new directory, git add directory, git add file, git commit! 
+//git add new_folder
+//git commit -a -m "comment about change" --> git push
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <title>EverYum...Retro!</title>
+  
+
+
+<link type="text/css" rel="stylesheet" href="css/openshift.css" />
+
+<script src="js/callYumly.js" language="javascript" type="application/javascript"></script>
+
+	
+</head>
+<body>
+	
+<div class="content">
+EverNote / Yumly: Vegan recipes on my old-school phone... powered by Deutsche Telekomm Tropo API	
+</div>
+
+
+</body>
+</html>
