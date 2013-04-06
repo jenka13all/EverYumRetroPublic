9f1b034759803f1e13df027616cf006fb344681c
jenka13all <jenka13all@gmail.com>
2013-04-07 00:29:38 +0200

    new implementation of ingredient-returning function, uses A's json code

--- libs/ingredient_calc_functions.php
+++ libs/ingredient_calc_functions.php
@@ -24,6 +24,103 @@ function return_recipe_array($data)
 	return $arr_recipes;
 }
 
+
+function return_shopping_list_obj($arr_my_items, $arr_recipes)
+{
+//$arr_my_items is a simple array of strings
+//$arr_recipes is an object 
+//foreach($arr_recipes->matches as $recipe) 
+
+
+//$arr_tobuy is an associative array with string as key (recipe name) and text as value (string items separated by commas)
+
+	$arr_data = array();
+	$matches  = count($arr_recipes);
+	$my_items_count = count($arr_my_items);
+
+	//for($i=0; $i<$matches; $i++)
+	foreach($arr_recipes->matches as $recipe) 
+	{	
+		$score = 0;
+		$recipe_name =  $recipe->recipeName;
+		$ingredients = implode(",", $recipe->ingredients);
+	
+//score number of matches between my fridge/grocery list and recipe ingredients
+		for($n=0; $n<$my_items_count; $n++)
+		{
+			$item_compare = trim($arr_my_items[$n]);
+			if(strpos($ingredients, $item_compare) !== false)
+			{
+				$score++;
+			}
+		}
+		
+		array_push($arr_data, array("recipe_name" => $recipe_name, "ingredients" => $ingredients, "score" => $score));
+	}
+
+	reset($arr_my_items);
+
+//sort $arr_data by score - highest first
+	$arr_scores = array();
+	foreach ($arr_data as $key => $row)
+	{
+    	$scores[$key] = $row['score'];
+	}
+
+	array_multisort($scores, SORT_DESC, $arr_data);
+
+//take only top 3 high scoring recipes
+	$arr_data = array_slice($arr_data, 0, 3, TRUE);
+
+
+//we want a "to buy" list now - the $ingredients (in $arr_data) that are NOT in $arr_fridge or in $arr_groceries ($arr_my_items)!
+	$tobuy = "";
+	$arr_tobuy = array();
+	$tobuy_index = 0;
+
+	foreach ($arr_data as $key => $value)
+	{
+	//echo "recipe = " . $value["recipe_name"] . "<br> score = " . $value["score"] . " ingredient matches <br> ingredient_string = " . $value["ingredients"] . "<p>";
+	
+		$tobuy = $value["ingredients"];
+	
+	//for each item in arr_fridge
+	//if item in fridge is in string $tobuy ($value["ingredients"]) 
+	//delete it from $tobuy
+	
+//exact match	
+		for($n=0; $n<$my_items_count; $n++)
+		{
+			$item_have = trim($arr_my_items[$n]);
+
+			if(strpos($tobuy, "," . $item_have . ",") !== false)
+			{
+			//echo "item deleted : $item_have<br>";
+				$tobuy = str_replace($item_have . ",", "", $tobuy);
+			}
+		}
+	
+		reset($arr_my_items);
+
+//close enough that might, might not have to buy - mark	it
+		for($n=0; $n<$my_items_count; $n++)
+		{
+			$item_have = trim($arr_my_items[$n]);
+
+			if(strpos($tobuy, $item_have) !== false)
+			{
+			//echo "item deleted : $item_have<br>";
+				$tobuy = str_replace($item_have, "<font color=\"red\">*$item_have*</font>", $tobuy);
+			}
+ 		}
+//echo "<p>tobuy = $tobuy<p>";
+		$arr_tobuy[$value["recipe_name"]] = $tobuy;
+
+	}
+
+	return $arr_tobuy;
+}
+
 function return_shopping_list($arr_my_items, $arr_recipes)
 {
 //$arr_tobuy is an associative array with string as key (recipe name) and text as value (string items separated by commas)
