3da1146a8a50ccd4dd04a8219cd51b34a91177d7
Armin Hackmann <armin@fruux.com>
2013-04-07 11:19:43 +0200

    limit is per account not application :P

--- config.php
+++ config.php
@@ -14,7 +14,7 @@ return array(
     'tropo.endpoint'    => 'https://tropo.developergarden.com/api/',
     // Daans Token
 #    'tropo.textToken'   => '5873626658576e52546a794d5849666b52444b626f484259536a646d5a51416b51474c42657869516f417a6d',
-    // Armins Token - alt (probably limit reached?)
+    // Armins Token
     'tropo.textToken'   => '6f66794b46416e614373684b6f5647635858466e43475273706a5057456a664a426166697a41706241666d41',
 
     // Yummly
