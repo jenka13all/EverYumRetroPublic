fb39312c295b2d09e8df54cc68d667bd64a53dd8
Armin Hackmann <armin@fruux.com>
2013-04-07 12:07:37 +0200

    hangup!?

--- php/receiving_call.php
+++ php/receiving_call.php
@@ -18,8 +18,8 @@ function app_continue() {
     @$result = new Result();
     $answer = $result->getValue();
     $tropo->say("You said " . $answer . ". You will receive a text message with three cooking suggestions based on the content of your fridge in the next 2 minutes.");
-#    $tropo->hangup();
     $tropo->on(array("event" => "continue", "next" => "ingredient_calc_final.php"));
+    $tropo->hangup();
     $tropo->RenderJson();
 }
 
