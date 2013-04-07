b4529af6faa2710eec6f0ebeb1e0523111c33168
Armin Hackmann <armin@fruux.com>
2013-04-07 11:12:38 +0200

    new token

--- config.php
+++ config.php
@@ -14,8 +14,9 @@ return array(
     'tropo.endpoint'    => 'https://tropo.developergarden.com/api/',
     // Daans Token
 #    'tropo.textToken'   => '5873626658576e52546a794d5849666b52444b626f484259536a646d5a51416b51474c42657869516f417a6d',
-    // Armins Token
-    'tropo.textToken'   => '6f66794b46416e614373684b6f5647635858466e43475273706a5057456a664a426166697a41706241666d41',
+    // Armins Token - alt (probably limit reached?)
+#    'tropo.textToken'   => '6f66794b46416e614373684b6f5647635858466e43475273706a5057456a664a426166697a41706241666d41',
+    'tropo.textToken'   => '6146414a59674e6975666f616f6376564b4668686d584b4c416447597961645a644d6a454c697a4846774e54',
 
     // Yummly
     'yummly.endpoint'   => 'http://api.yummly.com/v1/api/',
